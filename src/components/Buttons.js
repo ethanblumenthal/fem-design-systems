fe8dc6da95471c72adc0bce4e242c95b14bbe378
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-07-28 10:40:36 -0500

    Created colors, typeography, and themes.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,32 +1,29 @@
 import styled from "styled-components";
-
-const primaryBlue = "#030086";
+import { defaultTheme, typeScale, primaryFont } from "../utils";
 
 const Button = styled.button`
-  padding: 12px 24px;
-  font-size: 1rem;
+  padding: 8px 12px;
+  font-size: ${typeScale.paragraph};
   border-radius: 2px;
-	min-width: 100px
+  min-width: 100px;
   cursor: pointer;
-  font-family: "Roboto Mono", monospace;
+  font-family: ${primaryFont};
 `;
 
 const PrimaryButton = styled(Button)`
-  background-color: ${primaryBlue};
+  background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
 `;
 
 export const SecondaryButton = styled(Button)`
-  background: none;
-  border: 20px solid ${primaryBlue};
-  color: ${primaryBlue};
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
 `;
 
 export const TertiaryButton = styled(Button)`
-  background: none;
-  border: none;
-  color: ${primaryBlue};
+  border: 2px solid transparent;
+  color: ${defaultTheme.primaryColor};
 `;
 
 export default PrimaryButton;
